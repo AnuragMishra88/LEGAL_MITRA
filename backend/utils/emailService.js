0710cd107e4c4c3dfff6498f9e35a2b131505ab4
AnuragMishra88 <anurag.mishraa1210@gmail.com>
2025-11-02 12:23:53 +0530

    done

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 // Create transporter
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  port:2525,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
